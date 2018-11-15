9db5f043b54d5c6c3e341d23c3c874587dca9c47
Rafael Honda <rafael.honda@doxti.com.br>
2018-11-15 15:56:40 -0200

    setup heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
+localdbURI = 'mongodb://localhost:27017/TodoMongoose';
+
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoMongoose',
+    mlab = 'admin:admin001@ds045137.mlab.com:45137/node-todoapp',
+  };
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoMongoose'), { useNewUrlParser: true };
+mongoose.connect(db.localhost || db.mlab), { useNewUrlParser: true };
 
 module.exports = { mongoose };
