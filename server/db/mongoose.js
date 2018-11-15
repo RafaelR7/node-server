06c73297cd5f2cc8fc149957d084f87b2cd102ee
Rafael Honda <rafael.honda@doxti.com.br>
2018-11-15 17:03:25 -0200

    heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 
 localdbURI = 'mongodb://localhost:27017/TodoMongoose';
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoMongoose',
-    mlab = 'admin:admin001@ds045137.mlab.com:45137/node-todoapp',
-  };
-
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.MONGODB_URI || db.localhost ), { useNewUrlParser: true };
+console.log("MONGO--------", process.env.MONGODB_URI);
+mongoose.connect( process.env.MONGODB_URI || localdbURI );
 
 module.exports = { mongoose };
+
