527133c3834450d631df79965e3a073c0f2d64a0
Rafael Honda <rafael.honda@doxti.com.br>
2018-11-15 20:58:47 -0200

    mongodb_uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 localdbURI = 'mongodb://localhost:27017/TodoMongoose';
 
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.MONGODB_URI || localdbURI );
+mongoose.connect( process.env.MONGODB_URI);
 
 module.exports = { mongoose };
 
