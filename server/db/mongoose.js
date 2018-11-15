d564933eb3bc1c7ec70ed1024a706e5b06bc5069
Rafael Honda <rafael.honda@doxti.com.br>
2018-11-15 20:41:51 -0200

    Heroku setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 localdbURI = 'mongodb://localhost:27017/TodoMongoose';
 
 mongoose.Promise = global.Promise;
-console.log("MONGO--------", process.env.MONGODB_URI);
 mongoose.connect( process.env.MONGODB_URI || localdbURI );
 
 module.exports = { mongoose };
