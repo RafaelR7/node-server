66d02fc9a9ff33ee95b4b72aad6a990b63a3b97b
Rafael Honda <rafael.honda@doxti.com.br>
2018-11-16 23:08:39 -0200

    [add] Token

--- server/server.js
+++ server/server.js
@@ -9,9 +9,9 @@ const { User } = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
-
-//INSERT
 const port = process.env.PORT || 3000;
+
+//INSERT TODO
 app.post('/todos', (req, res) => {
    var todo = new Todo({
       text: req.body.text
@@ -90,6 +90,21 @@ app.patch('/todos/:id', (req, res) => {
       res.status(404).send();
    });
 });
+
+
+// POST /users
+app.post('/users', (req, res) => {
+   var body = _.pick(req.body, ['email', 'password']);
+   var user = new User(body);
+ 
+   user.save().then(() => {
+     return user.generateAuthToken();
+   }).then((token) => {
+     res.header('x-auth', token).send(user);
+   }).catch((e) => {
+     res.status(400).send(e);
+   })
+ });
 
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`);
